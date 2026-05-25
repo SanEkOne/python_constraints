a51dd816526e17926f55c4b65ee6d4ad9ee447aa
SanEk <tt.worm524@gmail.com>
2026-05-25 19:03:33 +0300

    admin panel

--- Models_python/app/admin.py
+++ Models_python/app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Author, Book
+from .models import Author, Book, Publishment, Country
 
 admin.site.register(Author)
 admin.site.register(Book)
+admin.site.register(Publishment)
+admin.site.register(Country)
--- Models_python/app/models.py
+++ Models_python/app/models.py
@@ -6,6 +6,11 @@ class Author(models.Model):
     name = models.CharField(_("Ім'я"), max_length=255)
     birth_year = models.IntegerField(_("Рік народження"), null=True, blank=True)
     rating = models.FloatField(_("Рейтинг"), default=0.0)
+    country = models.ForeignKey(
+        "Country", on_delete=models.CASCADE, related_name="authors", verbose_name=_("Країна"),
+        null=True,     
+        blank=True
+    )
 
     class Meta:
         verbose_name = _("Автор")
@@ -29,6 +34,14 @@ class Book(models.Model):
     price = models.DecimalField(_("Ціна"), max_digits=8, decimal_places=2)
     published_year = models.IntegerField(_("Рік видання"), null=True, blank=True)
     stock = models.IntegerField(_("Залишок на складі"), default=0)
+    publishment = models.ForeignKey(
+        "Publishment",
+        on_delete=models.CASCADE,
+        related_name="books",
+        verbose_name=_("Видавництво"),
+        null=True, blank=True
+    )
+
 
     class Meta:
         verbose_name = _("Книга")
@@ -42,3 +55,36 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Publishment(models.Model):
+    name = models.CharField(_("Назва видавництва"), max_length=255)
+    country = models.ForeignKey(
+        "Country", on_delete=models.CASCADE, related_name="publishments", verbose_name=_("Країна"),
+        null=True, blank=True
+    )
+
+    class Meta:
+        verbose_name = _("Видавництво")
+        verbose_name_plural = _("Видавництва")
+        ordering = ["name"]
+        indexes = [
+            models.Index(fields=["name"]),
+        ]
+
+    def __str__(self):
+        return self.name
+
+class Country(models.Model):
+    name = models.CharField(_("Назва країни"), max_length=255)
+
+    class Meta:
+        verbose_name = _("Країна")
+        verbose_name_plural = _("Країни")
+        ordering = ["name"]
+        indexes = [
+            models.Index(fields=["name"]),
+        ]
+
+    def __str__(self):
+        return self.name
